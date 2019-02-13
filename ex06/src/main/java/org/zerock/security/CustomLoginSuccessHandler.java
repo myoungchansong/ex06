11e67d52220f12df958d4d65cbe5431294348ce3
myoungchansong <audcks68@gmail.com>
2019-02-14 01:57:10 +0900

    스프링 시큐리티를 이용한 로그인 로그아웃 예제

---
+++ ex06/src/main/java/org/zerock/security/CustomLoginSuccessHandler.java
@@ -0,0 +1,69 @@
+package org.zerock.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler{
+	
+	
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication auth) throws IOException, ServletException {
+			// 사용자가 가진 모든 권한을 문자열로 체크
+	
+		log.warn("Login Success");
+		
+		List<String> roleNames = new ArrayList<>();
+	
+		auth.getAuthorities().forEach(authority -> {
+			
+			roleNames.add(authority.getAuthority());
+			
+		});
+		
+		log.warn("ROLE NAMES: "+ roleNames);
+		
+		if(roleNames.contains("ROLE_ADMIN")) {
+			
+			response.sendRedirect("/sample/admin");
+			return;
+			
+		}
+		
+		if (roleNames.contains("ROLE_MEMBER")) {
+			
+			response.sendRedirect("/sample/member");
+			
+			return;
+			
+		}
+	
+	
+	
+		response.sendRedirect("/");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+	}
+
+}
